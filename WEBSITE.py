637c90971c6d0865c5c9549df9069e59dae33248
Samraj26 <138555324+Samraj26@users.noreply.github.com>
2023-09-18 16:00:59 +0530

    Update WEBSITE.py

--- WEBSITE.py
+++ WEBSITE.py
@@ -7,14 +7,14 @@ from flask import Flask, render_template
 
 # FIREBASE CONFIG
 firebaseConfig = {
-  "apiKey": "AIzaSyDBlsxzjvoUCV6bgu6wwTXa2RfcYp4HT2c",
-  "authDomain": "fir-b8eb1.firebaseapp.com",
-  "databaseURL": "https://fir-b8eb1-default-rtdb.firebaseio.com",
-  "projectId": "fir-b8eb1",
-  "storageBucket": "fir-b8eb1.appspot.com",
-  "messagingSenderId": "135618758776",
-  "appId": "1:135618758776:web:7c7df7e53dcb709380858d",
-  "measurementId": "G-7G845RY86G"
+  "apiKey": 
+  "authDomain": 
+  "databaseURL":
+  "projectId": 
+  "storageBucket": 
+  "messagingSenderId": 
+  "appId": 
+  "measurementId": 
 }
 
 
