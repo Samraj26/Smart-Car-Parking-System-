d02db152b89529694e02b22f49f312f08f907d0f
Samraj26 <138555324+Samraj26@users.noreply.github.com>
2023-09-18 15:59:46 +0530

    Add files via upload

---
+++ WEBSITE.py
@@ -0,0 +1,68 @@
+import cv2
+import pickle
+import cvzone
+import numpy as np
+from pyrebase import pyrebase
+from flask import Flask, render_template
+
+# FIREBASE CONFIG
+firebaseConfig = {
+  "apiKey": "AIzaSyDBlsxzjvoUCV6bgu6wwTXa2RfcYp4HT2c",
+  "authDomain": "fir-b8eb1.firebaseapp.com",
+  "databaseURL": "https://fir-b8eb1-default-rtdb.firebaseio.com",
+  "projectId": "fir-b8eb1",
+  "storageBucket": "fir-b8eb1.appspot.com",
+  "messagingSenderId": "135618758776",
+  "appId": "1:135618758776:web:7c7df7e53dcb709380858d",
+  "measurementId": "G-7G845RY86G"
+}
+
+
+# INITIALIZING FIREBASE
+firebase = pyrebase.initialize_app(firebaseConfig)
+database = firebase.database()
+
+# FUNCTION TO CHECK FOR PARKING SPACES
+def checkParkingSpace(imgPro):
+    spaceCounter = 0
+
+    for pos in posList:
+        x,y = pos
+        imgCrop = imgPro[y:y+height,x:x+width]
+        count = cv2.countNonZero(imgCrop)
+        cvzone.putTextRect(img,str(count),(x,y+height-3), scale = 1, thickness=2, offset=0)
+
+        if count <800:
+            color = (0,255,0)
+            thickness = 5
+            spaceCounter +=1
+
+        else:
+            color = (0,0,255)
+            thickness = 2
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height),color, thickness)
+
+    cvzone.putTextRect(img, f'FREE: {spaceCounter}/{len(posList)}', (100,50), scale=5, thickness=5, offset=20, colorR=(0,200,0) )
+
+    global prev_space_counter
+    if spaceCounter != prev_space_counter:
+        # database.push({"space_counter": spaceCounter})
+        database.update({"space_counter": spaceCounter})
+        print(spaceCounter)
+        prev_space_counter = spaceCounter
+
+app = Flask(__name__)
+
+# ROUTE TO DISPLAY THE SPACE COUNTER ON A WEB PAGE
+@app.route("/")
+def index():
+    space_counter = database.child("space_counter").get().val()
+    return render_template("index.html", space_counter=space_counter)
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture('carPark.mp4')
+    with open('CarParkPos', 'rb') as f:
+        posList = pickle.load(f)
+    width, height = 107, 48
+    prev_space_counter = None
+    app.run(debug=True)
---
+++ finalCodeFB.py
@@ -0,0 +1,163 @@
+# # IMPORTING NECESSARY LIBRARIES
+# import cv2
+# import pickle
+# import cvzone
+# import numpy as np
+# from pyrebase import pyrebase
+# import streamlit as st
+
+# st.title("CAR PARKING")
+
+# # FIREBASE CONFIG
+# firebaseConfig = {
+#   "apiKey": "AIzaSyDBlsxzjvoUCV6bgu6wwTXa2RfcYp4HT2c",
+#   "authDomain": "fir-b8eb1.firebaseapp.com",
+#   "databaseURL": "https://fir-b8eb1-default-rtdb.firebaseio.com",
+#   "projectId": "fir-b8eb1",
+#   "storageBucket": "fir-b8eb1.appspot.com",
+#   "messagingSenderId": "135618758776",
+#   "appId": "1:135618758776:web:7c7df7e53dcb709380858d",
+#   "measurementId": "G-7G845RY86G"
+# }
+
+# # INITIALIZING FIREBASE
+# firebase = pyrebase.initialize_app(firebaseConfig)
+# database = firebase.database()
+
+# # FUNCTION TO CHECK FOR PARKING SPACES
+# def checkParkingSpace(imgPro):
+#     spaceCounter = 0
+
+#     for pos in posList:
+#         x,y = pos
+#         imgCrop = imgPro[y:y+height,x:x+width]
+#         count = cv2.countNonZero(imgCrop)
+#         cvzone.putTextRect(img,str(count),(x,y+height-3), scale = 1, thickness=2, offset=0)
+
+#         if count <800:
+#             color = (0,255,0)
+#             thickness = 5
+#             spaceCounter +=1
+
+#         else:
+#             color = (0,0,255)
+#             thickness = 2
+#         cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height),color, thickness)
+
+#     cvzone.putTextRect(img, f'FREE: {spaceCounter}/{len(posList)}', (100,50), scale=5, thickness=5, offset=20, colorR=(0,200,0) )
+
+#     global prev_space_counter
+#     if spaceCounter != prev_space_counter:
+#         # database.push({"space_counter": spaceCounter})
+#         database.update({"space_counter": spaceCounter})
+#         print(spaceCounter)
+#         prev_space_counter = spaceCounter
+#         st.text(f"Number of free parking spaces: {spaceCounter}")
+
+# # VIDEO FEED
+# cap = cv2.VideoCapture('carPark.mp4')
+
+# with open('CarParkPos', 'rb') as f:
+#     posList = pickle.load(f)
+
+# width, height = 107, 48
+
+# prev_space_counter = None
+
+# while True:
+    # if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+    #     cap.set(cv2.CAP_PROP_POS_FRAMES,0)
+    # success, img = cap.read()
+    # imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
+    # imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    #                                      cv2.THRESH_BINARY_INV, 25, 16)
+    # imgMedian = cv2.medianBlur(imgThreshold,5)
+    # kernal = np.ones((3, 3), np.uint8)
+    # imgDilate = cv2.dilate(imgMedian, kernal, iterations=1)
+
+    # checkParkingSpace(imgDilate)
+
+    # cv2.imshow("image",img)
+    # cv2.waitKey(3)
+
+############### NEW CODE FROM CHATGPT #################
+# IMPORTING NECESSARY LIBRARIES
+import cv2
+import pickle
+import cvzone
+import numpy as np
+from pyrebase import pyrebase
+
+# FIREBASE CONFIG
+firebaseConfig = {
+  "apiKey": "AIzaSyDBlsxzjvoUCV6bgu6wwTXa2RfcYp4HT2c",
+  "authDomain": "fir-b8eb1.firebaseapp.com",
+  "databaseURL": "https://fir-b8eb1-default-rtdb.firebaseio.com",
+  "projectId": "fir-b8eb1",
+  "storageBucket": "fir-b8eb1.appspot.com",
+  "messagingSenderId": "135618758776",
+  "appId": "1:135618758776:web:7c7df7e53dcb709380858d",
+  "measurementId": "G-7G845RY86G"
+}
+
+# INITIALIZING FIREBASE
+firebase = pyrebase.initialize_app(firebaseConfig)
+database = firebase.database()
+
+# FUNCTION TO CHECK FOR PARKING SPACES
+def checkParkingSpace(imgPro):
+    spaceCounter = 0
+
+    for pos in posList:
+        x,y = pos
+        imgCrop = imgPro[y:y+height,x:x+width]
+        count = cv2.countNonZero(imgCrop)
+        cvzone.putTextRect(img,str(count),(x,y+height-3), scale = 1, thickness=2, offset=0)
+
+        if count <800:
+            color = (0,255,0)
+            thickness = 5
+            spaceCounter +=1
+
+        else:
+            color = (0,0,255)
+            thickness = 2
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height),color, thickness)
+
+    cvzone.putTextRect(img, f'FREE: {spaceCounter}/{len(posList)}', (100,50), scale=5, thickness=5, offset=20, colorR=(0,200,0) )
+
+    global prev_space_counter
+    if spaceCounter != prev_space_counter:
+        # database.push({"space_counter": spaceCounter})
+        database.update({"space_counter": spaceCounter})
+        print(spaceCounter)
+        prev_space_counter = spaceCounter
+
+# VIDEO FEED
+cap = cv2.VideoCapture('carPark.mp4')
+
+with open('CarParkPos', 'rb') as f:
+    posList = pickle.load(f)
+
+width, height = 107, 48
+
+
+prev_space_counter = None
+
+while True:
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        cap.set(cv2.CAP_PROP_POS_FRAMES,0)
+    success, img = cap.read()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
+    imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                         cv2.THRESH_BINARY_INV, 25, 16)
+    imgMedian = cv2.medianBlur(imgThreshold,5)
+    kernal = np.ones((3, 3), np.uint8)
+    imgDilate = cv2.dilate(imgMedian, kernal, iterations=1)
+
+    checkParkingSpace(imgDilate)
+
+    cv2.imshow("image",img)
+    cv2.waitKey(3)
